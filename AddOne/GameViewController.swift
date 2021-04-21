c000ff09d1a25211b84965ce7646104443be6133
Jenn <reddunrogue@gmail.com>
2021-04-21 00:33:10 -0600

    Finished App

--- AddOne/GameViewController.swift
+++ AddOne/GameViewController.swift
@@ -8,23 +8,28 @@
 import UIKit
 
 class GameViewController: UIViewController {
+    var guessNumbers: [Int] = []
     var score = 0
-    var guessNumbers : [Int] = []
     var timer:Timer?
-    var seconds = 60
+    var seconds = 20
     
     @IBOutlet weak var timeLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
-    
     @IBOutlet weak var numberLabel: UILabel!
-    
     @IBOutlet weak var numberTextView: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        createRandomArray()
+        resetGame()
+    }
+    
+    func resetGame() {
+        
+        guessNumbers = createRandomArray()
         updateScoreLabel()
         updateNumberLabel()
         updateTimeLabel()
+        numberTextView.text = ""
+        
     }
     
     func updateScoreLabel() {
@@ -32,13 +37,38 @@ class GameViewController: UIViewController {
     }
     
     func updateNumberLabel() {
-        numberLabel?.text = arrayToString()
+        numberLabel?.text = arrayToString(array: guessNumbers)
     }
     
     @IBAction func inputFieldDidChange(_ sender: Any) {
-        guard let numberText = numberLabel?.text, let inputText = numberTextView?.text else {
+        guard let inputText = numberTextView?.text else {
             return
         }
+        
+        guard inputText.count == 4 else {
+            return
+        }
+        
+        var usersGuess = stringToArray(string: inputText)
+        
+        var isCorrect = true;
+        for i in 0..<4 {
+            if usersGuess[i] == 0 {
+                usersGuess[i] = 10
+            }
+            if(guessNumbers[i]+1 != usersGuess[i]) {
+                isCorrect = false
+                break
+            }
+        }
+        
+        if isCorrect {
+            score += 1
+        }
+        else {
+            score -= 1
+        }
+        
         if timer == nil {
             timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { timer in
                 if self.seconds == 0 {
@@ -49,21 +79,17 @@ class GameViewController: UIViewController {
                 }
             }
         }
+        resetGame()
     }
     
-    func createRandomArray() {
-        for i in 0..<4 {
-            guessNumbers.append(Int.random(in: 0...9))
+    func createRandomArray() -> [Int] {
+        var array : [Int] = []
+        for _ in 0..<4 {
+            array.append(Int.random(in: 0...9))
         }
+        return array
     }
     
-    func arrayToString() -> String {
-        var result = ""
-        for i in 0..<4 {
-            result += "\(guessNumbers[i])"
-        }
-        return result
-    }
     
     func updateTimeLabel() {
         
@@ -78,13 +104,33 @@ class GameViewController: UIViewController {
         timer?.invalidate()
         timer = nil
         let alert = UIAlertController(title: "Time's Up!", message: "Your time is up! You got a score of \(score) points. Awesome!", preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "OK, start new game", style: .default, handler: nil))
+        alert.addAction(UIAlertAction(title: "OK, start new game", style: .default, handler: {_ in
+            self.score = 0
+            self.seconds = 20
+            self.resetGame()
+        }))
         self.present(alert, animated: true, completion: nil)
-        createRandomArray()
-        updateScoreLabel()
-        updateNumberLabel()
-        updateTimeLabel()
+        
     }
     
+    func arrayToString(array: [Int]) -> String {
+        var result = ""
+        for i in 0..<4 {
+            result += "\(array[i])"
+        }
+        return result
+    }
+    
+    func stringToArray(string: String) -> [Int] {
+        var array : [Int] = []
+        for char in string {
+            array.append(Int(String(char)) ?? 0)
+        }
+        
+        for i in 0..<4 {
+            print(array[i])
+        }
+        return array
+    }
 }
 
